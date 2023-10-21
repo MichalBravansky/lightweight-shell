9b4b5a149d8d531ccac8638f51da5e75151b668e
suhas-13 <hariharansuhas@gmail.com>
2023-10-21 20:56:46 +0100

    add convert arg list

--- src/commands/command.py
+++ src/commands/command.py
@@ -1,13 +1,25 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 class Command(ABC):
     def __init__(self, command_name, command_description):
         self.command_name = command_name
         self.command_description = command_description
 
+    @staticmethod
+    def convert_arg_list_to_arg_dict(self, args):
+        arg_dict = defaultdict(str)
+        for arg in args:
+            arg_dict[arg.arg_name] = arg.arg_value
+        return arg_dict
+
     # Assumes that the command has been pre-validated by the parser. 
     @abstractmethod
     def execute(self, args):
+        process_args()
+        folder = None
+        flag1 = False
+        {"folder": }
         pass
 
     def help(self):
