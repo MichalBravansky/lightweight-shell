56f927c3508ab563a58c25573fa31c05bc8efd39
Michal Bravanský <michal.bravansky@o2.cz>
2023-12-11 15:52:22 +0000

    Test for decorator added

---
+++ test/test_decorator.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+from src.shell_parser.executors import Call
+from utils.unsafe_decorator import UnsafeDecorator
+
+class TestDecorator(unittest.TestCase):
+
+    def test_valid_call(self):
+        result = UnsafeDecorator(Call("echo", ["foo"])).evaluate()
+
+    def test_invalid_call(self):
+        try:
+            result = UnsafeDecorator(Call("ls", [0.4])).evaluate()
+        except:
+            self.fail("Call should not have failed")
